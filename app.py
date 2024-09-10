610f30480a3becf0e2d84043a5161ddbcb9beea8
CristianCamiloRoaQuiroga <cristiancamilo.roa.contractor@bbva.com>
2024-09-10 14:32:21 -0500

    modificacion en la rama develop

---
+++ app.py
@@ -0,0 +1,2 @@
+Todo lo que hagamos en esta rama develop, es como el trabajo local, cuando mergeamos develop a main es trabajo productivo..
+-Ejercicio git, bajar cambios, hacer una rama, modificar el carchivo app.py, subir cambios, y mergear los cambios de la rama creada, en la rama develop
